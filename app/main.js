f1cefd79a18cfb20ad32e034e6bf33f614e5b297
jbsnyk <justin.bukstel@snyk.io>
2024-10-02 16:04:55 -0400

    Delete app directory

--- app/main.js
+++
@@ -1,7 +0,0 @@
-const expression = process.argv.slice(2).join('').concat(';');
-try {
-  console.log(eval(expression));
-} catch {
-  console.log('Error: Unable to evaluation ${expression}. Maybe try again');
-  process.exit(1);
-}
